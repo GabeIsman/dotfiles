7b3d653c694f02c5175a966ba3b579c1675c6cc5
Gabe Isman <gabe.isman@gmail.com>
2016-09-13 12:17:23 -0700

    Switch from rvm to rbenv.

--- fish/config.fish
+++ fish/config.fish
@@ -9,9 +9,6 @@ set PATH /usr/local/heroku/bin $PATH
 ### Add psql to PATH
 set PATH $PATH /Applications/Postgres.app/Contents/Versions/9.4/bin
 
-### Add vim to path so locally compiled version takes precedence over Apple's
-set PATH /usr/local/bin/vim $PATH
-
 # Setting PATH for Python 3.5
 # The orginal version is saved in .bash_profile.pysave
 set PATH /Library/Frameworks/Python.framework/Versions/3.5/bin $PATH
@@ -19,10 +16,13 @@ set PATH /Library/Frameworks/Python.framework/Versions/3.5/bin $PATH
 ### Add TeX to the path
 set PATH $PATH /Library/TeX/Distributions/Programs/texbin
 
-rvm use 2.3.0
-
 # Virtualfish
 export VIRTUALFISH_HOME=$HOME/Envs
 export PROJECT_HOME=$HOME/Code
 export VIRTUALFISH_DEFUALT_PYTHON=/Library/Frameworks/Python.framework/Versions/3.5/bin/python3
 eval (python -m virtualfish compat_aliases)
+
+# Look into https://github.com/oh-my-fish/oh-my-fish
+
+# Load rbenv
+status --is-interactive; and . (rbenv init -|psub)
