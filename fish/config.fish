5b97b9bc46ed00f6ad13504fe537399c21160b20
Gabe Isman <gabe.isman@gmail.com>
2017-02-08 11:37:37 -0500

    Fix errors and use lates postgres.

--- fish/config.fish
+++ fish/config.fish
@@ -6,15 +6,26 @@ source ~/.config/fish/aliases.fish
 
 ### Added by the Heroku Toolbelt
 set PATH /usr/local/heroku/bin $PATH
-### Add psql to PATH
-set PATH $PATH /Applications/Postgres.app/Contents/Versions/9.4/bin
+
+### Add psql and pg_config to PATH
+set POSTGRES_PATH /Applications/Postgres.app/Contents/Versions/latest/bin
+if test -d $POSTGRES_PATH
+  set PATH $PATH $POSTGRES_PATH
+end
 
 # Setting PATH for Python 3.5
 # The orginal version is saved in .bash_profile.pysave
-set PATH /Library/Frameworks/Python.framework/Versions/3.5/bin $PATH
+set PYTHON3_PATH /Library/Frameworks/Python.framework/Versions/3.5/bin
+if test -d $PYTHON3_PATH
+  set PATH $PATH $PYTHON3_PATH
+end
 
 ### Add TeX to the path
-set PATH $PATH /Library/TeX/Distributions/Programs/texbin
+
+set TEX_PATH /Library/TeX/Distributions/Programs/texbin
+if test -e $TEX_PATH
+  set PATH $PATH $TEX_PATH
+end
 
 # Move /usr/local/bin ahead of /usr/bin to allow overriding system binaries
 set PATH /usr/local/bin $PATH
@@ -28,5 +39,5 @@ eval (python -m virtualfish compat_aliases)
 # Look into https://github.com/oh-my-fish/oh-my-fish
 
 # Load rbenv and set up path
-set PATH /Users/admin/.rbenv/shims:/Users/admin/.rbenv/bin $PATH
+set PATH /Users/admin/.rbenv/shims $PATH
 status --is-interactive; and . (rbenv init -|psub)
