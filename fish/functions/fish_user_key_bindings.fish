cba92a4eda6f6992bba1ae2282b5c700c09fff12
Gabe Isman <gabe.isman@gmail.com>
2023-04-13 11:39:34 -0400

    Add fish keybinding for partial completion.

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,4 +1,5 @@
 function fish_user_key_bindings
   bind --mode insert --sets-mode default jj backward-char force-repaint
   bind --mode insert \cf accept-autosuggestion
+  bind --mode insert \ce forward-word
 end
